41b39ffb6412e0053540c872f763ac8c753fcf68
Rovane Moura <rovane@macbook-pro-de-rovane.local>
2019-03-12 16:50:51 -0300

    well it's something

--- MuBank/userAccount.swift
+++ MuBank/userAccount.swift
@@ -14,6 +14,8 @@ struct MuUser {
     var monthlyIncome: Int
     var password: String
     
+    var invoice: Double
+    
     
     func configureMuAccount(){
         for _ in 0..<15{
@@ -39,6 +41,6 @@ struct MuUser {
     
 }
 
-var myUser = MuUser(usarname: "Anderson", email: "aaa.com", bankNumber: 323, agency: 1111, account: 72849, monthlyIncome: 1100, password: "senha")
+var myUser = MuUser(usarname: "Anderson", email: "aaa.com", bankNumber: 323, agency: 1111, account: 72849, monthlyIncome: 1100, password: "senha", invoice: 10.2)
 
 
