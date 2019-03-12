83dc21daf9c21f9c9de89ca8ee754644eee110df
Henrique Conte <henrique.conte@inf.ufrgs.br>
2019-03-12 16:51:57 -0300

    will test graphics

--- MuBank/userAccount.swift
+++ MuBank/userAccount.swift
@@ -26,17 +26,65 @@ struct MuUser {
     
     func configureCard(){
         
+        for _ in 0..<15{
+            print(" ")
+        }
+        
+        print("Invoice Maturity:               08/04      >")
+        print("Ajust limit:                    $400,00    >")
+        print("Information about your bank interets: 14%  >")
+        print("Travel alert:                    ON")
+    
     }
     
     func configureApp(){
         
+        for _ in 0..<15 {
+            print(" ")
+        }
+        
+        print("Localization history                                  ON")
+        print("App protection")
+        print("Ask for the user password when openning the app:      ON")
     }
     
     func recentlyBought(){
         
+        for _ in 0..<15 {
+            print(" ")
+        }
+        
+        print("         ^")
+        print("        / \\ ")
+        print("     __/   \\__/ ")
+        
+        
+        print("Restaurant:           MAR 13")
+        print("  PizzaHut")
+        print("  $21,50   \n")
+        
+        print("Supermarket:          FEV 18")
+        print("  Walmart")
+        print("  #118,92")
+        
     }
     
     
+}
+
+
+func upperMenu(){
+    
+    print(     "---------------")
+    print(     "|  -|||- _ | -_|")
+    print(     "| _-|- -|| _-|-|")
+    print(     "| ||- -_-| -| _|")
+    print(     "| ----| || - | |")
+    print(     "---------------")
+    
+    print("Bank \(myUser.bankNumber) -  Mu payments S.A.")
+    print("          Agency: \(myUser.agency)    ")
+    print("         Account: \(myUser.account)    ")
 }
 
 var myUser = MuUser(usarname: "Anderson", email: "aaa.com", bankNumber: 323, agency: 1111, account: 72849, monthlyIncome: 1100, password: "senha")
