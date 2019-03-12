64720c77da8aa748732868eb2d336ee423aa5604
Henrique Conte <henrique.conte@inf.ufrgs.br>
2019-03-12 15:50:00 -0300

    user part 1

--- MuBank/userAccount.swift
+++ MuBank/userAccount.swift
@@ -1,9 +1,44 @@
-
 import Foundation
 
-class NuUser {
+
+
+
+struct MuUser {
     
-    var bank: Int = 0
+    let usarname: String
+    let email: String
+    let bankNumber: Int
+    let agency: Int
+    let account: Int
+    
+    var monthlyIncome: Int
+    var password: String
+    
+    
+    func configureMuAccount(){
+        for _ in 0..<15{
+            print("")
+        }
+        
+        print("Receive Salary               >")
+        print("Cancel MuAccount             >")
+    }
+    
+    func configureCard(){
+        
+    }
+    
+    func configureApp(){
+        
+    }
+    
+    func recentlyBought(){
+        
+    }
+    
+    
 }
 
-var cebola = "🍊"
+var myUser = MuUser(usarname: "Anderson", email: "aaa.com", bankNumber: 323, agency: 1111, account: 72849, monthlyIncome: 1100, password: "senha")
+
+
